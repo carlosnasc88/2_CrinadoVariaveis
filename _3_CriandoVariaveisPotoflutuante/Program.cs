3a3505437bb6083e828b559ff4361fb762863f34
carlosnasc88 <carlosnasc88@hotmail.com>
2021-08-18 20:59:53 -0300

    Adicionar arquivos de projeto.

---
+++ _3_CriandoVariaveisPotoflutuante/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _3_CriandoVariaveisPotoflutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execultado projeto3 criando variaveis ponto flutuante");
+
+            double salario;
+            salario = 1200.70;
+
+            Console.WriteLine(salario);
+
+            double idade;
+            idade = 15 / 2.0;
+            Console.WriteLine(idade);
+
+            idade = 5.0 / 3;
+            Console.WriteLine("5.0 /3 = " + idade);
+
+
+            Console.WriteLine("A execução acabou. Tecle Enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ _4_ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Excutando o projeto 4");
+
+            double salario;
+            salario = 1200.5;
+
+            //O inet é variavel que suporta valores de até 32nt
+            int salarioEminteiro;
+            salarioEminteiro = (int) salario;
+
+            Console.WriteLine(salarioEminteiro);
+            //O long é variavel que suporta valores de até 64bt
+            long idade;
+            idade = 13000000000;
+            Console.WriteLine(idade);
+
+            //O short é variavel que suporta valores de até 32nt
+            short quantidadeProduto;
+            quantidadeProduto = 150;
+            Console.WriteLine(quantidadeProduto);
+
+            float altuta;
+            altuta = 1.85f;
+            Console.WriteLine(altuta);
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
